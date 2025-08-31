b686d354b0db74185f44d5c051c232d50b08a288
Vikrammodi18 <suchit.sbd.9631@gmail.com>
2025-09-01 00:04:36 +0530

    changes

--- lib/feature/search/view/discover_screen.dart
+++ lib/feature/search/view/discover_screen.dart
@@ -16,10 +16,75 @@ class DiscoverScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          
-        ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Material(
+                      borderRadius: BorderRadius.circular(20),
+                      elevation: 4,
+                      shadowColor: Color(0xffFAFAFA),
+                      child: TextFormField(
+                        style: TextStyle(color: Color(0xff777E90)),
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 12,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                          hint: Text(
+                            "Search",
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Color(0xff777E90),
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          prefixIcon: Icon(
+                            Icons.search_rounded,
+                            color: Color(0xff777E90),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 15),
+                  Material(
+                    elevation: 4,
+                    shadowColor: Colors.grey,
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color(0xffFAFAFA),
+                      ),
+                      child: Icon(Icons.menu),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Image.asset("assets/images/searchBanner1.png"),
+              SizedBox(height: 20),
+              Image.asset("assets/images/searchBanner2.png"),
+              SizedBox(height: 20),
+              Image.asset("assets/images/searchBanner3.png"),
+              SizedBox(height: 20),
+              Image.asset("assets/images/searchBanner4.png"),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gem_store/bottom_navigation.dart';
-import 'package:gem_store/feature/home/view/home_screen.dart';
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
